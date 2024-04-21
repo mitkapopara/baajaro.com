fe6af93f2021eb00ee154d72e40489791c58f598
mitkapopara <mitkapopara1234.com>
2024-04-21 17:39:40 +0100

    Updated dependencies and fixed deprecations

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import "../styles/HeroSection.css";
 
@@ -7,27 +7,31 @@ function HeroSection() {
   const apiKey = process.env.REACT_APP_API_KEY;
   console.log(process.env.REACT_APP_API_KEY);
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        fetchWeather(position.coords.latitude, position.coords.longitude);
-      },
-      () => {
-        console.error("Geolocation not supported or permission denied");
+  const fetchWeather = useCallback(
+    async (lat, lon) => {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+        );
+        setWeather(response.data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
       }
-    );
-  }, []);
+    },
+    [apiKey]
+  ); // Make sure apiKey is included as a dependency
 
-  const fetchWeather = async (lat, lon) => {
-    try {
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
-      );
-      setWeather(response.data);
-    } catch (error) {
-      console.error("Error fetching weather data:", error);
-    }
-  };
+  useEffect(() => {
+    const success = (position) => {
+      fetchWeather(position.coords.latitude, position.coords.longitude);
+    };
+
+    const error = () => {
+      console.error("Geolocation not supported or permission denied");
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, [fetchWeather]); // Include fetchWeather in the dependency array
 
   const getWeatherIcon = (condition) => {
     switch (condition) {
