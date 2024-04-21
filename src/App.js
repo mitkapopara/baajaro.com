4492ffefe824d6da5efa41b60035c81139532796
mitkapopara <mitkapopara1234.com>
2024-04-21 15:15:08 +0100

    Updated project settings and cleaned up unused files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HomePage from "./components/HomePage";
-import "./styles/HomePage.css";
+import "./styles/index.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <HomePage />
     </div>
   );
